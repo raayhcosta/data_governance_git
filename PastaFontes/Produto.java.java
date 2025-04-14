1d6607df6efefe2ade789a7f25fedef50ee8ded5
raayhcosta <raissanc.228@gmail.com>
2025-04-14 11:53:27 -0300

    alteração na variavel peso

--- PastaFontes/Produto.java.java
+++ PastaFontes/Produto.java.java
@@ -2,27 +2,27 @@
 //Inclui e consulta dados de produto
 public class Produto {
 
-	private double peso;
+	private int peso;
 	private double altura;
 	
 	public Produto() {
 		
 	}
 	
-	public Produto(double peso, double altura) {
+	public Produto(int peso, double altura) {
 		this.peso=peso;
 		this.altura=altura;
 	}
 	
-	public double getPeso() {
+	public int getPeso() {
 		return peso;
 	}
 	
-	public void setPeso(double peso) {
+	public void setPeso(int peso) {
 		this.peso = peso;
 	}
 	
-	public double getAltura() {
+	public int getAltura() {
 		return altura;
 	}
 	
